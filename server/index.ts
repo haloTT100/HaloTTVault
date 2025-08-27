6962e7d018afb5b277b8879a1187e0feb52a5ac1
n0ll22 <remibende@gmail.com>
2025-08-27 21:35:29 +0200

    .env changes

--- server/index.ts
+++ server/index.ts
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json());
 
 // Initialize SQLite database
-const db = new sqlite3.Database("./halottvaultdb.sqlite", (err) => {
+const db = new sqlite3.Database(`./${process.env.DB_NAME}.sqlite`, (err) => {
   if (err) {
     console.error("Failed to connect to SQLite database:", err);
   } else {
